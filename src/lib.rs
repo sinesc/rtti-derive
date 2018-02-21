d564aad22ae00a4037c571bbe7b921676db759b7
Dennis Möhlmann <dmo479@gmail.com>
2018-02-21 21:40:23 +0100

    fix missing use

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ extern crate syn;
 #[macro_use]
 extern crate quote;
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 
 #[proc_macro_derive(RTTI, attributes(HelloWorldName))]
 pub fn macro_rtti(input: TokenStream) -> TokenStream {
@@ -32,12 +33,14 @@ fn translate_visibility(vis: &syn::Visibility) -> syn::Ident {
 }
 
 fn impl_rtti(ast: &syn::DeriveInput) -> quote::Tokens {
+
     let ident = ast.ident;
     let name = ast.ident.to_string();
     let visibility = translate_visibility(&ast.vis);
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+    let dummy_const = syn::Ident::new(&format!("_IMPL_RTTI_FOR_{}", ident), Span::def_site());
 
-    let result = if let syn::Data::Struct(ref data) = ast.data {
+    let body = if let syn::Data::Struct(ref data) = ast.data {
         if let syn::Fields::Named(ref fields) = data.fields {
 
             // handle structs with named members
@@ -52,31 +55,27 @@ fn impl_rtti(ast: &syn::DeriveInput) -> quote::Tokens {
             }).collect();
 
             quote! {
-                impl #impl_generics RTTI for #ident #ty_generics #where_clause  {
-                    fn rtti() -> Type {
-                        Type::Struct(Struct {
-                            name: #name.to_string(),
-                            vis: Visibility::#visibility,
-                            fields: {
-                                let mut fields = Vec::new();
-                                let dummy: #ident #impl_generics = unsafe { ::std::mem::uninitialized() };
-                                #(
-                                    fields.push((#names.to_string(), Field {
-                                        vis: Visibility::#visibilities,
-                                        offset: {
-                                            let dummy_ref = &dummy;
-                                            let field_ref = &dummy.#idents;
-                                            (field_ref as *const _ as usize) - (dummy_ref as *const _ as usize)
-                                        },
-                                        ty: Box::new(<#types>::rtti())
-                                    }));
-                                )*
-                                std::mem::forget(dummy);
-                                fields
-                            }
-                        })
+                Type::Struct(Struct {
+                    name: #name.to_string(),
+                    vis: Visibility::#visibility,
+                    fields: {
+                        let mut fields = Vec::new();
+                        let dummy: #ident #impl_generics = unsafe { ::std::mem::uninitialized() };
+                        #(
+                            fields.push((#names.to_string(), Field {
+                                vis: Visibility::#visibilities,
+                                offset: {
+                                    let dummy_ref = &dummy;
+                                    let field_ref = &dummy.#idents;
+                                    (field_ref as *const _ as usize) - (dummy_ref as *const _ as usize)
+                                },
+                                ty: Box::new(<#types>::rtti())
+                            }));
+                        )*
+                        std::mem::forget(dummy);
+                        fields
                     }
-                }
+                })
             }
 
         } else if let syn::Fields::Unnamed(ref fields) = data.fields {
@@ -92,38 +91,47 @@ fn impl_rtti(ast: &syn::DeriveInput) -> quote::Tokens {
             }).collect();
 
             quote! {
-                impl #impl_generics RTTI for #ident #ty_generics #where_clause  {
-                    fn rtti() -> Type {
-                        Type::Tuple(Tuple {
-                            name: #name.to_string(),
-                            vis: Visibility::#visibility,
-                            fields: {
-                                let mut fields = Vec::new();
-                                let dummy: #ident #impl_generics = unsafe { ::std::mem::uninitialized() };
-                                #(
-                                    fields.push(Field {
-                                        vis: Visibility::#visibilities,
-                                        offset: {
-                                            let dummy_ref = &dummy;
-                                            let field_ref = &(dummy.#indices);
-                                            (field_ref as *const _ as usize) - (dummy_ref as *const _ as usize)
-                                        },
-                                        ty: Box::new(<#types>::rtti())
-                                    });
-                                )*
-                                std::mem::forget(dummy);
-                                fields
-                            }
-                        })
+                Type::Tuple(Tuple {
+                    name: #name.to_string(),
+                    vis: Visibility::#visibility,
+                    fields: {
+                        let mut fields = Vec::new();
+                        let dummy: #ident #impl_generics = unsafe { ::std::mem::uninitialized() };
+                        #(
+                            fields.push(Field {
+                                vis: Visibility::#visibilities,
+                                offset: {
+                                    let dummy_ref = &dummy;
+                                    let field_ref = &(dummy.#indices);
+                                    (field_ref as *const _ as usize) - (dummy_ref as *const _ as usize)
+                                },
+                                ty: Box::new(<#types>::rtti())
+                            });
+                        )*
+                        std::mem::forget(dummy);
+                        fields
                     }
-                }
+                })
             }
         } else {
             panic!("#[derive(RTTI)] NYI unit struct.");
         }
     } else {
         panic!("#[derive(RTTI)] NYI non-struct..");
+    };
+
+    let tmp = quote! {
+        #[allow(non_upper_case_globals)]
+        const #dummy_const: () = {
+            extern crate rtti;
+            use rtti::*;
+            impl #impl_generics RTTI for #ident #ty_generics #where_clause  {
+                fn rtti() -> Type {
+                    #body
+                }
+            }
+        };
     }
-    ;result
-    //;panic!(result.to_string())
+    ;tmp
+    //;panic!(tmp.to_string());
 }
